60e5596a1b3d87f061dff469ca5afbd4b9195ad0
LaaraibAhmed <90887574+LaaraibAhmed@users.noreply.github.com>
2022-06-02 04:26:02 +0500

    Update view-ticket-admin.php

--- account/view-ticket-admin.php
+++ account/view-ticket-admin.php
@@ -33,7 +33,7 @@ if($continue){
   <title>Ticket No. <?php echo $ticket_id.' - '. $type;?></title>
 
   <!-- Favicons-->
-<<<<<<< HEAD
+
   <link rel="icon" href="images/favicon/favicon-32x32.png" sizes="32x32">
   <!-- Favicons-->
   <link rel="apple-touch-icon-precomposed" href="images/favicon/apple-touch-icon-152x152.png">
@@ -42,7 +42,7 @@ if($continue){
   <meta name="msapplication-TileImage" content="images/favicon/mstile-144x144.png">
   <!-- For Windows Phone -->
 
-=======
+
   <link rel="icon" href="../images/mylogo.png" sizes="32x32">
         <!-- Favicons-->
         <link rel="apple-touch-icon-precomposed" href="../images/mylogo.png">
@@ -50,7 +50,7 @@ if($continue){
         <meta name="msapplication-TileColor" content="#00bcd4">
         <meta name="msapplication-TileImage" content="../images/mylogo.png">
         <!-- For Windows Phone -->
->>>>>>> 26cb1f1 (Laaraib Commit)
+
 
   <!-- CORE CSS-->
   <link href="css/materialize.min.css" type="text/css" rel="stylesheet" media="screen,projection">
