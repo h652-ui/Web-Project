c15853dafd07df9bb51e448323a408cd42e1e4bb
LaaraibAhmed <90887574+LaaraibAhmed@users.noreply.github.com>
2022-06-02 04:30:24 +0500

    Update all-orders.php

--- account/all-orders.php
+++ account/all-orders.php
@@ -13,15 +13,7 @@ if ($_SESSION['admin_sid'] == session_id()) {
         <title>All orders</title>
 
 
-        <!-- Favicons-->
-        <link rel="icon" href="images/favicon/favicon-32x32.png" sizes="32x32">
-        <!-- Favicons-->
-        <link rel="apple-touch-icon-precomposed" href="images/favicon/apple-touch-icon-152x152.png">
-        <!-- For iPhone -->
-        <meta name="msapplication-TileColor" content="#00bcd4">
-        <meta name="msapplication-TileImage" content="images/favicon/mstile-144x144.png">
-        <!-- For Windows Phone -->
-
+      
 
  <!-- Favicons-->
  <link rel="icon" href="../images/mylogo.png" sizes="32x32">
