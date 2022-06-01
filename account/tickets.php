fa9cca026676e8d8546e865da6307019ab4e864d
LaaraibAhmed <ahmadlaaraib@gmail.com>
2022-06-02 02:27:05 +0500

    Laaraib Commit

--- account/tickets.php
+++ account/tickets.php
@@ -340,9 +340,8 @@ if ($_SESSION['customer_sid'] == session_id()) {
       <footer class="page-footer">
         <div class="footer-copyright">
           <div class="container">
-            <span>Copyright © 2017 <a class="grey-text text-lighten-4" href="#" target="_blank">Students</a> All rights reserved.</span>
-            <span class="right"> Design and Developed by <a class="grey-text text-lighten-4" href="#">Students</a></span>
-          </div>
+          <span>Copyright © 2022 <a class="grey-text text-lighten-4" href="#" target="_blank">Students</a> All rights reserved.</span>
+        <span class="right"> Design and Developed by <a class="grey-text text-lighten-4" href="#">Nust Students</a></span></div>
         </div>
       </footer>
       <!-- END FOOTER -->
